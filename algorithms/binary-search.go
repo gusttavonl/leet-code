84d4e2f14d95e0d01bca46e976c21baa36441a24
GustavoNoronha0 <admin@gustavonoronha.com.br>
2024-01-29 11:51:53 -0300

    refactor: improve calc from pivot/medium

--- algorithms/binary-search.go
+++ algorithms/binary-search.go
@@ -1,16 +1,17 @@
 func search(nums []int, target int) int {
-	left := 0
-	right := len(nums) - 1
+	left, right := 0, len(nums)-1
 
 	for left <= right {
-		mid := (left + right) / 2
+		mid := left + (right-left)/2
 
 		if nums[mid] == target {
 			return mid
-		} else if nums[mid] < target {
-			left = mid + 1
-		} else {
+		}
+
+		if target < nums[mid] {
 			right = mid - 1
+		} else {
+			left = mid + 1
 		}
 	}
 
